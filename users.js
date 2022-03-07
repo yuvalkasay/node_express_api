e727c46844d37ac73325898767dfd5f3aeeb9a2d
yuvalkasay <57451645+yuvalkasay@users.noreply.github.com>
2022-03-07 22:50:20 +0200

    Add files via upload

--- users.js
+++ users.js
@@ -1,24 +1,49 @@
-import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid"; // create id the every user (library)
 
-import {
-  createUser,
-  getUsers,
-  getUser,
-  deleteUser,
-  updateUser,
-} from "../constrollers/users.js";
+let users = [];
 
-const router = express.Router();
+export const getUsers = (req, res) => {
+  res.send(users);
+};
 
-router.get("/", getUsers);
+export const createUser = (req, res) => {
+  const user = req.body;
 
-router.post("/", createUser);
+  const userId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
-router.get("/:id", getUser);
+  const userWithId = { ...user, id: userId };
 
-router.delete("/:id", deleteUser);
+  users.push(userWithId);
 
-router.patch("/:id", updateUser);
+  res.send(`User with the name: ${user.firstName}, added to the database.`);
+};
 
-export default router;
+export const getUser = (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id == id);
+
+  // get the data only to that specific profile
+  res.send(foundUser);
+};
+
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`USer with the id: ${id} deleted from the database.`);
+};
+
+export const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+
+  const user = users.find((user) => user.id == id);
+
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+
+  res.send(`User with the id ${id} has been updated.`);
+};
